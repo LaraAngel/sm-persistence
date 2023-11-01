ef09a3c7a1d0c813ff8d6801bd1c27aa14af35c4
Angel Lara <lara.angel.js@gmail.com>
2023-10-31 18:05:32 -0600

    init

---
+++ src/main/java/com/schoolmanager/persistence/SmPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolmanager/persistence/SmPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
